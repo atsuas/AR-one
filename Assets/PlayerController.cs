9d9cdaf93e937988f367998e2de480069c0c089e
as <>
2021-04-04 05:26:17 +0900

    アニメーションの実装、Player.csの作成

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float x;
+    float z;
+    public float moveSpeed = 2;
+
+    Rigidbody rb;
+    Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // 方向を取得
+        Vector3 direction = transform.position + new Vector3(x, 0, z) * moveSpeed;
+        // directionの方向に向く
+        transform.LookAt(direction);
+        rb.velocity = new Vector3(x, 0, z) * moveSpeed;
+        animator.SetFloat("Speed", rb.velocity.magnitude);
+    }
+}
